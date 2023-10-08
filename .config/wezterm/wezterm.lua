fb2cceee60c56c2108434add0397added9bd92b4
Andis Spriņķis <spr.andis@protonmail.com>
2023-10-08 22:33:42 +0300

    ok

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,18 +1,18 @@
-local wezterm = require 'wezterm';
-
+local wezterm = require('wezterm')
 local cfg_local_status, cfg_local = pcall(require, 'cfg_local')
 
 return {
   color_scheme = 'vscode-dark',
+  cursor_blink_rate = 0,
+  enable_tab_bar = false,
   font = wezterm.font('Cascadia Code PL'),
   font_size = cfg_local_status and cfg_local['font_size'] or 12.0,
+  freetype_load_target = 'Light',
   line_height = 1.29,
-  enable_tab_bar = false,
-  cursor_blink_rate = 0,
   window_padding = {
     left = 4,
     right = 4,
     top = 4,
     bottom = 4,
-  }
+  },
 }
