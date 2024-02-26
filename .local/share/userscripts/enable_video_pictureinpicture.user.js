76daac63a77ecab8b4b067658c1fb62109d777fa
Andis Spriņķis <spr.andis@protonmail.com>
2024-02-26 08:08:56 +0200

    ok

--- .local/share/userscripts/enable_video_pictureinpicture.user.js
+++ .local/share/userscripts/enable_video_pictureinpicture.user.js
@@ -3,12 +3,12 @@
 // @namespace   https://andis.work
 // @match       *://*/*
 // @grant       none
-// @version     0.1.0
+// @version     0.2.0
 // @copyright   Licensed under the MIT License. This script is a code fork of the Firefox add-on "Enable Picture-in-Picture" authored by Sven - https://addons.mozilla.org/en-US/firefox/addon/enable-picture-in-picture/ .
 // @author      Andis Spriņķis
 // @description Automatically enables Picture-in-Picture (PiP) for all web videos.
-// @downloadURL https://github.com/andis-sprinkis/nix-user-config/raw/master/.local/share/usersripts/enable_video_pictureinpicture.user.js
-// @updateURL   https://github.com/andis-sprinkis/nix-user-config/raw/master/.local/share/usersripts/enable_video_pictureinpicture.user.js
+// @downloadURL https://github.com/andis-sprinkis/nix-user-config/raw/master/.local/share/userscripts/enable_video_pictureinpicture.user.js
+// @updateURL   https://github.com/andis-sprinkis/nix-user-config/raw/master/.local/share/userscripts/enable_video_pictureinpicture.user.js
 // ==/UserScript==
 
 function adjustVideoElement(video) {
