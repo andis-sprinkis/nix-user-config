36df86ac462baadd479018d9a5915d24a89387fe
Andis Spriņķis <spr.andis@protonmail.com>
2024-09-18 05:28:48 +0300

    ok

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -16,7 +16,6 @@ user_pref("browser.fixup.alternate.enabled", false);
 user_pref("browser.formfill.enable", false);
 user_pref("browser.fullscreen.autohide", true);
 user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
-user_pref("layout.testing.overlay-scrollbars.always-visible", true)
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 user_pref("browser.newtabpage.activity-stream.default.sites", "");
@@ -89,6 +88,7 @@ user_pref("geo.provider.use_gpsd", false);
 user_pref("identity.fxaccounts.enabled", false);
 user_pref("intl.accept_languages", "en-US, en");
 user_pref("javascript.use_us_english_locale", true);
+user_pref("layout.testing.overlay-scrollbars.always-visible", true)
 user_pref("media.autoplay.default", 5);
 user_pref("media.videocontrols.picture-in-picture.improved-video-controls.enabled", true);
 user_pref("media.videocontrols.picture-in-picture.respect-disablePictureInPicture", false);
