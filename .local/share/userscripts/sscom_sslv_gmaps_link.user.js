b1fe5dca102e30b8e653dfd4820a7a18a1a178ab
Andis Spriņķis <andis@sprinkis.com>
2025-11-16 22:24:09 +0200

    ok

--- .local/share/userscripts/sscom_sslv_gmaps_link.user.js
+++ .local/share/userscripts/sscom_sslv_gmaps_link.user.js
@@ -5,9 +5,9 @@
 // @match       https://www.ss.lv/*
 // @grant       none
 // @run-at      document-end
-// @version     0.4.0
+// @version     0.5.0
 // @author      Andis Spriņķis
-// @description Display the location services links of Google Maps, Apple Maps, OpenStreetMap, BalticMaps in the ss.com, ss.lv listings.
+// @description Display the location services links of Google Maps, Apple Maps, OpenStreetMap, BalticMaps and the geo URI in the ss.com, ss.lv listings.
 // @downloadURL https://raw.githubusercontent.com/andis-sprinkis/nix-user-config/refs/heads/master/.local/share/userscripts/sscom_sslv_gmaps_link.user.js
 // @updateURL   https://raw.githubusercontent.com/andis-sprinkis/nix-user-config/refs/heads/master/.local/share/userscripts/sscom_sslv_gmaps_link.user.js
 // ==/UserScript==
@@ -41,6 +41,9 @@
 
   const urlAppleMaps = `https://maps.apple.com/place?coordinate=${lat}%2C${lon}`;
 
+  // LĢIA Kartes
+  // const urlLgia = `https://kartes.lgia.gov.lv/karte/?x=0.0&y=0.0&zoom=11&basemap=blankmap`
+
   // "Karšu slāņi"
   // - "Ortofoto karte", "ar nosaukumiem"
   // - "Interešu punkti" (all)
@@ -48,6 +51,8 @@
   // - "Administratīvais iedalījums" (all)
   // Right click, "Norādes uz" ("Maršruta izveide")
   const urlBalticMaps = `https://balticmaps.eu/lv/c%5F%5F%5F${lat}-${lon}-18/w%5F%5F%5Fdriving-${lat},${lon}/bl%5F%5F%5Fpl/nosaukumi/kadastrs/admin%5Fied/q%5F%5F%5F`;
+
+  const uriGeo = `geo:${lat},${lon}`;
 
   console.info(
     `Latitude, longitude:
@@ -63,7 +68,11 @@ Apple Maps:
 ${urlAppleMaps}
 
 BalticMaps:
-${urlBalticMaps}`,
+${urlBalticMaps}
+
+geo URI:
+${uriGeo}
+`,
   );
 
   const html = (strings, ...values) => String.raw({ raw: strings }, ...values);
@@ -85,6 +94,11 @@ ${urlBalticMaps}`,
       >
       <span class="td15"
         >[<a href="${urlBalticMaps}" class="ads_opt_link_map" target="blank" rel="noreferrer noopener">BalticMaps</a
+        >]</span
+      >
+
+      <span class="td15"
+        >[<a href="${uriGeo}" class="ads_opt_link_map" target="blank" rel="noreferrer noopener">geo URI</a
         >]</span
       >`,
   );
