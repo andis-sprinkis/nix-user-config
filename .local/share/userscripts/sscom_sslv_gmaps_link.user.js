cfb8a8cbc81c021d37321d2b89687e4f6ab14de7
Andis Spriņķis <andis@sprinkis.com>
2025-11-16 13:40:31 +0200

    ok

--- .local/share/userscripts/sscom_sslv_gmaps_link.user.js
+++ .local/share/userscripts/sscom_sslv_gmaps_link.user.js
@@ -5,33 +5,49 @@
 // @match       https://www.ss.lv/*
 // @grant       none
 // @run-at      document-end
-// @version     0.1.0
+// @version     0.2.0
 // @author      Andis Spriņķis
 // @description Display the Google Maps location link in the ss.com, ss.lv listings
 // ==/UserScript==
 
-const mapLinkElement = document.getElementById("mnu_map");
+(() => {
+  const mapLinkElement = document.getElementById("mnu_map");
 
-if (!mapLinkElement) return;
+  if (!mapLinkElement) return;
 
-const onClickAttr = mapLinkElement.getAttribute("onclick");
+  const onClickAttr = mapLinkElement.getAttribute("onclick");
 
-if (!onClickAttr) return;
+  if (!onClickAttr) {
+    console.error(`The map link element 'onclick' attribute to match the coordinates from is not found.`);
+    return;
+  }
 
-const latLonMatch = onClickAttr.match(/c=(-?\d+\.\d+),\s?(-?\d+.\d+),\s?\d+'\)/);
+  const latLonMatch = onClickAttr.match(/c=(-?\d+\.\d+),\s?(-?\d+.\d+),\s?\d+'\)/);
 
-if (!latLonMatch) return;
+  if (latLonMatch?.length !== 3) {
+    console.error(`No latitude and longitude match within the map link element 'onclick' attribute.`);
+    return;
+  }
 
-const lat = latLonMatch[1];
-const lon = latLonMatch[2];
+  const lat = latLonMatch[1];
+  const lon = latLonMatch[2];
 
-if (!lat.length && !lon.length) return;
+  const urlGmaps = `https://www.google.com/maps?t=k&q=loc:${lat}+${lon}`;
+  // Does not insert the map pinpoint: `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`
 
-const urlGmaps = `https://www.google.com/maps?t=k&q=loc:${lat}+${lon}`;
+  console.info(
+    `Latitude and longitude match:
+${lat} ${lon}
+Google Maps URL:
+${urlGmaps}`,
+  );
 
-// Does not insert the map pinpoint: `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`
+  const html = (strings, ...values) => String.raw({ raw: strings }, ...values);
 
-mapLinkElement.parentElement.insertAdjacentHTML(
-  "afterend",
-  ` <span class="td15">[<a href="${urlGmaps}" class="ads_opt_link_map" target="blank" rel="noreferrer noopener">Google Maps</a>]</span>`,
-);
+  mapLinkElement.parentElement.insertAdjacentHTML(
+    "afterend",
+    html` <span class="td15"
+      >[<a href="${urlGmaps}" class="ads_opt_link_map" target="blank" rel="noreferrer noopener">Google Maps</a>]</span
+    >`,
+  );
+})();
