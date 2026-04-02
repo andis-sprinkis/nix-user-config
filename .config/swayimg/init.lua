43b52952174d89af30c052ecbf07c946f47f1d16
Andis Spriņķis <andis@sprinkis.com>
2026-04-02 21:01:22 +0300

    ok

--- .config/swayimg/init.lua
+++ .config/swayimg/init.lua
@@ -1,11 +1,41 @@
 local S = swayimg
+local gallery = S.gallery
+local imagelist = S.imagelist
+local set_title = S.set_title
+local slideshow = S.slideshow
+local text = S.text
+local viewer = S.viewer
 
-S.text.set_size(16)
 S.enable_overlay(false)
-S.imagelist.enable_adjacent(true)
+S.enable_decoration(true)
+S.set_dnd_button('MouseExtra')
+imagelist.enable_adjacent(true)
+text.hide()
+text.set_size(16)
+viewer.set_default_scale('optimal')
 
--- set a custom window title in gallery mode
-S.gallery.on_image_change(function()
-  local image = S.gallery.get_image()
-  S.set_title("Gallery: " .. image.path)
-end)
+gallery.on_image_change(
+  function()
+    local img = gallery.get_image()
+
+    set_title(img.path .. " [" .. img.index .. "/" .. imagelist.size() .. "]")
+  end
+)
+
+viewer.on_image_change(
+  function()
+    local img = viewer.get_image()
+
+    set_title(img.path ..
+      " " .. img.width .. "x" .. img.height .. " [" .. img.index .. "/" .. imagelist.size() .. "]")
+  end
+)
+
+slideshow.on_image_change(
+  function()
+    local img = slideshow.get_image()
+
+    set_title(img.path ..
+      " " .. img.width .. "x" .. img.height .. " [" .. img.index .. "/" .. imagelist.size() .. "]")
+  end
+)
