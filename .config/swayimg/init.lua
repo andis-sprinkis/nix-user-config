6668af9229c946df079c48f0874ebc44fe0e5582
Andis Spriņķis <andis@sprinkis.com>
2026-06-25 14:52:55 +0300

    ok

--- .config/swayimg/init.lua
+++ .config/swayimg/init.lua
@@ -1,13 +1,14 @@
 local S = swayimg
-local g = S.gallery
-local bg = g.on_key
-local mg = g.on_mouse
 local imglist = S.imagelist
-local title = S.set_title
-local s = S.slideshow
 local txt = S.text
+local g = S.gallery
 local v = S.viewer
+local s = S.slideshow
+local bg = g.on_key
+local bs = s.on_key
 local bv = v.on_key
+local mg = g.on_mouse
+local ms = s.on_mouse
 local mv = v.on_mouse
 
 local thumb_size_default = 128
@@ -34,111 +35,158 @@ v.enable_loop(false)
 v.limit_preload(3)
 v.set_default_scale('fit')
 v.set_drag_button('MouseRight')
+s.limit_preload(3)
+s.set_default_scale('fit')
+s.set_drag_button('MouseRight')
 
 local nop = function() end
 
-local function vtitle()
-  local img = v.get_image()
+local function title()
+  local mode = S.get_mode()
+
+  local img = S[mode].get_image()
 
   if img == nil then
-    title("swayimg")
+    S.set_title("swayimg")
     return
   end
 
   local wsize = S.get_window_size()
 
   local fname = ""
-
   for part in img.path:gmatch("([^/]+)") do fname = part end
 
   local wtitle = (wsize.width < 800) and fname or img.path
 
-  title(
+  if mode == 'gallery' then
+    S.set_title(
+      '[' .. img.index .. '/' .. imglist.size() .. ']' ..
+      ' ' ..
+      wtitle
+    )
+
+    return
+  end
+
+  S.set_title(
     '[' .. img.index .. '/' .. imglist.size() .. ']' ..
     ' ' ..
     wtitle ..
     ' ' ..
-    '[' .. math.floor(v.get_scale() * 100) .. '%' .. ']' ..
+    '[' .. math.floor(S[mode].get_scale() * 100) .. '%' .. ']' ..
     ' ' ..
     '[' .. img.width .. 'x' .. img.height .. ']'
   )
 end
 
-local function gtitle()
-  local img = g.get_image()
+local function zoomreset()
+  local mode = S.get_mode()
 
-  if img == nil then
-    title("swayimg")
+  if mode == 'gallery' then
+    g.set_thumb_size(thumb_size_default)
+    title()
+
     return
   end
 
-  local wsize = S.get_window_size()
+  S[mode].reset()
+  title()
+end
 
-  local fname = ""
-  for part in img.path:gmatch("([^/]+)") do fname = part end
+local function zoomreal()
+  S[S.get_mode()].set_fix_scale('real')
+  title()
+end
 
-  local wtitle = (wsize.width < 800) and fname or img.path
+local function zoomin()
+  local mode = S.get_mode()
 
-  title(
-    '[' .. img.index .. '/' .. imglist.size() .. ']' ..
-    ' ' ..
-    wtitle
+  if mode == 'gallery' then
+    g.set_thumb_size(g.get_thumb_size() + 10)
+    return
+  end
+
+  local scale = S[mode].get_scale()
+  S[mode].set_abs_scale(scale + scale / 10)
+  title()
+end
+
+local function zoomout()
+  local mode = S.get_mode()
+
+  if mode == 'gallery' then
+    g.set_thumb_size(g.get_thumb_size() - 10)
+    return
+  end
+
+  local scale = S[mode].get_scale()
+  S[mode].set_abs_scale(scale - scale / 10)
+  title()
+end
+
+local function mzoomin()
+  local mode = S.get_mode()
+
+  if mode == 'gallery' then
+    g.set_thumb_size(g.get_thumb_size() + 10)
+    return
+  end
+
+  local mpos = S.get_mouse_pos()
+  local scale = S[mode].get_scale()
+  S[mode].set_abs_scale(scale + scale / 10, mpos.x, mpos.y)
+  title()
+end
+
+local function mzoomout()
+  local mode = S.get_mode()
+
+  if mode == 'gallery' then
+    g.set_thumb_size(g.get_thumb_size() - 10)
+    return
+  end
+
+  local mpos = S.get_mouse_pos()
+  local scale = S[mode].get_scale()
+  S[mode].set_abs_scale(scale - scale / 10, mpos.x, mpos.y)
+  title()
+end
+
+for _, mode in pairs({ 'gallery', 'viewer', 'slideshow' }) do
+  S[mode].on_image_change(
+    function()
+      title()
+      txt.set_status(S[mode].get_image().index .. '/' .. imglist.size())
+    end
   )
-end
 
-g.on_image_change(
-  function()
-    gtitle()
-    txt.set_status(g.get_image().index .. '/' .. imglist.size())
-  end
-)
+  S[mode].bind_reset()
 
-v.on_image_change(
-  function()
-    vtitle()
-    txt.set_status(v.get_image().index .. '/' .. imglist.size())
-  end
-)
 
-local function vzoomreset()
-  v.reset()
-  vtitle()
-end
-
-local function vzoomorig()
-  v.set_fix_scale('real')
-  vtitle()
+  S[mode].on_key('equal', zoomin)
+  S[mode].on_key('plus', zoomin)
+  S[mode].on_key('minus', zoomout)
+  S[mode].on_key('0', zoomreset)
+  S[mode].on_key('i', function() if txt.visible() then txt.hide() else txt.show() end end)
 end
 
 S.on_window_resize(
   function()
-    local mode = S.get_mode()
-    if (mode == 'viewer') then
-      vzoomreset()
-      vtitle()
+    if (S.get_mode() == 'gallery') then
+      title()
       return
     end
 
-    if (mode == 'gallery') then
-      gtitle()
-    end
+    zoomreset()
   end
 )
 
-local function mode_viewer()
-  S.set_mode('viewer')
-end
-
-local function mode_gallery()
-  S.set_mode('gallery')
-end
+local function mode_viewer() S.set_mode('viewer') end
+local function mode_gallery() S.set_mode('gallery') end
+local function mode_slideshow() S.set_mode('slideshow') end
 
 local function file_manager_desktop()
-  local mode = S.get_mode()
-  local imgpath
-
-  if mode == 'viewer' then imgpath = v.get_image().path end
-  if mode == 'gallery' then imgpath = g.get_image().path end
+  local imgpath = S[S.get_mode()].get_image().path
 
   if (imgpath == nil) then return end
 
@@ -150,11 +198,7 @@ local function file_manager_desktop()
 end
 
 local function reopen()
-  local mode = S.get_mode()
-  local imgpath
-
-  if mode == "viewer" then imgpath = v.get_image().path end
-  if mode == "gallery" then imgpath = g.get_image().path end
+  local imgpath = S[S.get_mode()].get_image().path
 
   if (imgpath == nil) then return end
 
@@ -166,11 +210,7 @@ local function reopen()
 end
 
 local function open_with_menu_desktop()
-  local mode = S.get_mode()
-  local imgpath
-
-  if mode == "viewer" then imgpath = v.get_image().path end
-  if mode == "gallery" then imgpath = g.get_image().path end
+  local imgpath = S[S.get_mode()].get_image().path
 
   if (imgpath == nil) then return end
 
@@ -182,11 +222,7 @@ local function open_with_menu_desktop()
 end
 
 local function pager_desktop()
-  local mode = S.get_mode()
-  local imgpath
-
-  if mode == "viewer" then imgpath = v.get_image().path end
-  if mode == "gallery" then imgpath = g.get_image().path end
+  local imgpath = S[S.get_mode()].get_image().path
 
   if (imgpath == nil) then return end
 
@@ -197,30 +233,11 @@ local function pager_desktop()
   )
 end
 
-g.bind_reset()
-v.bind_reset()
-s.bind_reset()
-
-local function gzoomin()
-  g.set_thumb_size(g.get_thumb_size() + 10)
-end
-
-local function gzoomout()
-  g.set_thumb_size(g.get_thumb_size() - 10)
-end
-
-local function gzoomreset()
-  g.set_thumb_size(thumb_size_default)
-end
-
 bg('e', file_manager_desktop)
-bg('equal', gzoomin)
-bg('plus', gzoomin)
-bg('minus', gzoomout)
-bg('0', gzoomreset)
-bg('Ctrl-0', gzoomreset)
+bg('Ctrl-0', zoomreset)
 bg('Return', mode_viewer)
 bg('Space', mode_viewer)
+bg('s', mode_slideshow)
 bg('Shift-space', mode_viewer)
 bg('h', function() g.switch_image('left') end)
 bg('j', function() g.switch_image('down') end)
@@ -242,7 +259,6 @@ bg('Shift-g', function() g.switch_image('last') end)
 bg('f', S.set_fullscreen)
 bg('Escape', S.exit)
 bg('q', S.exit)
-bg('i', function() if txt.visible() then txt.hide() else txt.show() end end)
 
 mg('ScrollUp', function() g.switch_image('pgup') end)
 mg('ScrollDown', function() g.switch_image('pgdown') end)
@@ -252,17 +268,17 @@ mg('Ctrl-MouseLeft', mode_viewer)
 mg('Ctrl-MouseRight', mode_viewer)
 mg('MouseLeft', mode_viewer)
 mg('MouseMiddle', mode_viewer)
-mg('Ctrl-ScrollLeft', gzoomin)
-mg('Ctrl-ScrollRight', gzoomout)
-mg('Ctrl-ScrollUp', gzoomin)
-mg('Ctrl-ScrollDown', gzoomout)
-mg('Ctrl-MouseMiddle', gzoomreset)
+mg('Ctrl-ScrollLeft', mzoomin)
+mg('Ctrl-ScrollRight', mzoomout)
+mg('Ctrl-ScrollUp', mzoomin)
+mg('Ctrl-ScrollDown', mzoomout)
+mg('Ctrl-MouseMiddle', zoomreset)
 mg('MouseRight', nop)
-mg('MouseRight-ScrollLeft', gzoomin)
-mg('MouseRight-ScrollRight', gzoomout)
-mg('MouseRight-ScrollUp', gzoomin)
-mg('MouseRight-ScrollDown', gzoomout)
-mg('MouseRight-MouseMiddle', gzoomreset)
+mg('MouseRight-ScrollLeft', mzoomin)
+mg('MouseRight-ScrollRight', mzoomout)
+mg('MouseRight-ScrollUp', mzoomin)
+mg('MouseRight-ScrollDown', mzoomout)
+mg('MouseRight-MouseMiddle', zoomreset)
 mg('MouseRight-MouseMiddle-ScrollUp', nop)
 mg('MouseRight-MouseMiddle-ScrollDown', nop)
 mg('MouseRight-MouseLeft', mode_viewer)
@@ -274,18 +290,6 @@ mg('MouseLeft-ScrollLeft', nop)
 mg('MouseLeft-ScrollRight', nop)
 mg('MouseLeft-ScrollUp', nop)
 mg('MouseLeft-ScrollDown', nop)
-
-local function vzoomin()
-  local scale = v.get_scale()
-  v.set_abs_scale(scale + scale / 10)
-  vtitle()
-end
-
-local function vzoomout()
-  local scale = v.get_scale()
-  v.set_abs_scale(scale - scale / 10)
-  vtitle()
-end
 
 local function vpanl()
   local wsize = S.get_window_size()
@@ -311,19 +315,17 @@ local function vpand()
   v.set_abs_position(icoord.x, math.floor(icoord.y - wsize.height / 10))
 end
 
-local function img_next()
+local function vimg_next()
   v.switch_image('next')
 end
 
-local function img_prev()
+local function vimg_prev()
   v.switch_image('prev')
 end
 
 bv('e', file_manager_desktop)
-bv('equal', vzoomin)
-bv('plus', vzoomin)
-bv('minus', vzoomout)
 bv('Return', mode_gallery)
+bv('s', mode_slideshow)
 bv('h', vpanl)
 bv('j', vpand)
 bv('k', vpanu)
@@ -332,89 +334,73 @@ bv('Left', vpanl)
 bv('Down', vpand)
 bv('Up', vpanu)
 bv('Right', vpanr)
-bv('0', vzoomreset)
-bv('Ctrl-0', vzoomorig)
+bv('Ctrl-0', zoomreal)
 bv(
   'bracketleft',
   function()
     v.rotate(270)
-    vzoomreset()
+    zoomreset()
   end
 )
 bv(
   'bracketright',
   function()
     v.rotate(90)
-    vzoomreset()
+    zoomreset()
   end
 )
 bv('Shift-braceleft', v.flip_vertical)
 bv('Shift-braceright', v.flip_horizontal)
-bv('n', img_next)
-bv('p', img_prev)
+bv('n', vimg_next)
+bv('p', vimg_prev)
 bv('r', reopen)
 bv('Shift-o', open_with_menu_desktop)
 bv('Shift-p', pager_desktop)
-bv('Space', img_next)
-bv('Shift-space', img_prev)
+bv('Space', vimg_next)
+bv('Shift-space', vimg_prev)
 bv('g', function() v.switch_image('first') end)
 bv('Shift-g', function() v.switch_image('last') end)
 bv(
   'Shift-w',
   function()
     v.set_fix_scale('width')
-    vtitle()
+    title()
   end
 )
 bv(
   'Ctrl-w',
   function()
     v.set_fix_scale('height')
-    vtitle()
+    title()
   end
 )
-bv('w', vzoomreset)
+bv('w', zoomreset)
 bv('Shift-r', v.reload)
 bv('f', S.set_fullscreen)
 bv('Escape', S.exit)
 bv('q', S.exit)
-bv('i', function() if txt.visible() then txt.hide() else txt.show() end end)
 
-local function mvzoomin()
-  local mpos = S.get_mouse_pos()
-  local scale = v.get_scale()
-  v.set_abs_scale(scale + scale / 10, mpos.x, mpos.y)
-  vtitle()
-end
-
-local function mvzoomout()
-  local mpos = S.get_mouse_pos()
-  local scale = v.get_scale()
-  v.set_abs_scale(scale - scale / 10, mpos.x, mpos.y)
-  vtitle()
-end
-
-mv('MouseLeft', img_next)
-mv('MouseRight-MouseLeft', img_prev)
-mv('Ctrl-MouseLeft', img_next)
-mv('Ctrl-MouseRight-MouseLeft', img_prev)
+mv('MouseLeft', vimg_next)
+mv('MouseRight-MouseLeft', vimg_prev)
+mv('Ctrl-MouseLeft', vimg_next)
+mv('Ctrl-MouseRight-MouseLeft', vimg_prev)
 mv('Ctrl-MouseRight', nop)
-mv('ScrollUp', img_prev)
-mv('ScrollDown', img_next)
-mv('ScrollLeft', img_prev)
-mv('ScrollRight', img_next)
+mv('ScrollUp', vimg_prev)
+mv('ScrollDown', vimg_next)
+mv('ScrollLeft', vimg_prev)
+mv('ScrollRight', vimg_next)
 mv('MouseMiddle', mode_gallery)
-mv('Ctrl-ScrollLeft', mvzoomin)
-mv('Ctrl-ScrollRight', mvzoomout)
-mv('Ctrl-ScrollUp', mvzoomin)
-mv('Ctrl-ScrollDown', mvzoomout)
-mv('Ctrl-MouseMiddle', vzoomreset)
+mv('Ctrl-ScrollLeft', mzoomin)
+mv('Ctrl-ScrollRight', mzoomout)
+mv('Ctrl-ScrollUp', mzoomin)
+mv('Ctrl-ScrollDown', mzoomout)
+mv('Ctrl-MouseMiddle', zoomreset)
 mv('MouseRight', nop)
-mv('MouseRight-ScrollLeft', mvzoomin)
-mv('MouseRight-ScrollRight', mvzoomout)
-mv('MouseRight-ScrollUp', mvzoomin)
-mv('MouseRight-ScrollDown', mvzoomout)
-mv('MouseRight-MouseMiddle', vzoomreset)
+mv('MouseRight-ScrollLeft', mzoomin)
+mv('MouseRight-ScrollRight', mzoomout)
+mv('MouseRight-ScrollUp', mzoomin)
+mv('MouseRight-ScrollDown', mzoomout)
+mv('MouseRight-MouseMiddle', zoomreset)
 mv('MouseRight-MouseMiddle-ScrollUp', nop)
 mv('MouseRight-MouseMiddle-ScrollDown', nop)
 mv('MouseMiddle-ScrollLeft', nop)
@@ -425,3 +411,124 @@ mv('MouseLeft-ScrollLeft', nop)
 mv('MouseLeft-ScrollRight', nop)
 mv('MouseLeft-ScrollUp', nop)
 mv('MouseLeft-ScrollDown', nop)
+
+local function spanl()
+  local wsize = S.get_window_size()
+  local icoord = s.get_position()
+  s.set_abs_position(math.floor(icoord.x + wsize.width / 10), icoord.y)
+end
+
+local function spanr()
+  local wsize = S.get_window_size()
+  local icoord = s.get_position()
+  s.set_abs_position(math.floor(icoord.x - wsize.width / 10), icoord.y)
+end
+
+local function spanu()
+  local wsize = S.get_window_size()
+  local icoord = s.get_position()
+  s.set_abs_position(icoord.x, math.floor(icoord.y + wsize.height / 10))
+end
+
+local function spand()
+  local wsize = S.get_window_size()
+  local icoord = s.get_position()
+  s.set_abs_position(icoord.x, math.floor(icoord.y - wsize.height / 10))
+end
+
+local function simg_next()
+  s.switch_image('next')
+end
+
+local function simg_prev()
+  s.switch_image('prev')
+end
+
+bs('e', file_manager_desktop)
+bs('Return', mode_gallery)
+bs('s', mode_slideshow)
+bs('h', spanl)
+bs('j', spand)
+bs('k', spanu)
+bs('l', spanr)
+bs('Left', spanl)
+bs('Down', spand)
+bs('Up', spanu)
+bs('Right', spanr)
+bs('Ctrl-0', zoomreal)
+bs(
+  'bracketleft',
+  function()
+    s.rotate(270)
+    zoomreset()
+  end
+)
+bs(
+  'bracketright',
+  function()
+    s.rotate(90)
+    zoomreset()
+  end
+)
+bs('Shift-braceleft', s.flip_vertical)
+bs('Shift-braceright', s.flip_horizontal)
+bs('n', simg_next)
+bs('p', simg_prev)
+bs('r', reopen)
+bs('Shift-o', open_with_menu_desktop)
+bs('Shift-p', pager_desktop)
+bs('Space', simg_next)
+bs('Shift-space', simg_prev)
+bs('g', function() s.switch_image('first') end)
+bs('Shift-g', function() s.switch_image('last') end)
+bs(
+  'Shift-w',
+  function()
+    s.set_fix_scale('width')
+    title()
+  end
+)
+bs(
+  'Ctrl-w',
+  function()
+    s.set_fix_scale('height')
+    title()
+  end
+)
+bs('w', zoomreset)
+bs('Shift-r', s.reload)
+bs('f', S.set_fullscreen)
+bs('Escape', S.exit)
+bs('q', S.exit)
+
+ms('MouseLeft', simg_next)
+ms('MouseRight-MouseLeft', simg_prev)
+ms('Ctrl-MouseLeft', simg_next)
+ms('Ctrl-MouseRight-MouseLeft', simg_prev)
+ms('Ctrl-MouseRight', nop)
+ms('ScrollUp', simg_prev)
+ms('ScrollDown', simg_next)
+ms('ScrollLeft', simg_prev)
+ms('ScrollRight', simg_next)
+ms('MouseMiddle', mode_gallery)
+ms('Ctrl-ScrollLeft', mzoomin)
+ms('Ctrl-ScrollRight', mzoomout)
+ms('Ctrl-ScrollUp', mzoomin)
+ms('Ctrl-ScrollDown', mzoomout)
+ms('Ctrl-MouseMiddle', zoomreset)
+ms('MouseRight', nop)
+ms('MouseRight-ScrollLeft', mzoomin)
+ms('MouseRight-ScrollRight', mzoomout)
+ms('MouseRight-ScrollUp', mzoomin)
+ms('MouseRight-ScrollDown', mzoomout)
+ms('MouseRight-MouseMiddle', zoomreset)
+ms('MouseRight-MouseMiddle-ScrollUp', nop)
+ms('MouseRight-MouseMiddle-ScrollDown', nop)
+ms('MouseMiddle-ScrollLeft', nop)
+ms('MouseMiddle-ScrollRight', nop)
+ms('MouseMiddle-ScrollUp', nop)
+ms('MouseMiddle-ScrollDown', nop)
+ms('MouseLeft-ScrollLeft', nop)
+ms('MouseLeft-ScrollRight', nop)
+ms('MouseLeft-ScrollUp', nop)
+ms('MouseLeft-ScrollDown', nop)
